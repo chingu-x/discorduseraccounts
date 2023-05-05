746f5c6dcba17d3c47c3723fceec2013be8eb90b
jdmedlock <jdmedlock@gmail.com>
2023-05-05 11:10:44 -0500

    Feature: Add basic user extract logic

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 import { Command } from 'commander'
 import { initDotEnv } from './src/utils/helpers.js'
-import dotenv from 'dotenv'
+import * as dotenv from 'dotenv'
 import extractDiscordUsers from './src/extractDiscordUsers.js'
 
-const program = new Command()
-
-//initDotEnv('./')
-dotenv.config()
-
 // Process a request to extract user accounts from the Chingu Discord server
-program 
-  .command('extract <source>')
-  .description('Extract user accounts from Discord server')
-  .action(async (source, options, command) => {
- 
-    try {
-      if (command._name === 'extract' && source.toLowerCase() === 'users') {
-        await extractDiscordUsers()
+(async () => {
+  const program = new Command()
+  dotenv.config()
+  program 
+    .command('extract <source>')
+    .description('Extract user accounts from Discord server')
+    .action(async (source, options, command) => {
+      try {
+        if (command._name === 'extract' && source.toLowerCase() === 'users') {
+          await extractDiscordUsers()
+        }
+        // process.exit(0)
       }
-      process.exit(0)
-    }
-    catch (err) {
-      console.log(err)
-      process.exit(0)
-    }
-  })
+      catch (err) {
+        console.log(err)
+        process.exit(0)
+      }
+    })
 
-  program.parse(process.argv)
+    program.parse(process.argv)
+  })()
--- src/extractDiscordUsers.js
+++ src/extractDiscordUsers.js
@@ -1,17 +1,34 @@
 import { Client, GatewayIntentBits } from 'discord.js'
 
-
 const extractDiscordUsers = async () => {
-const client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMembers,
-    ],
-  })
-  const list = client.guilds.cache.get(process.env.GUILD_ID)
-  console.log('list: ', list)
-  list.members.cache.forEach(member => console.log(member.user.username));
-  client.login(process.env.DISCORD_TOKEN)
+  const client = new Client({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+      ],
+    })
+  console.log('client.guilds: ', client.guilds)
+
+  const login = await client.login(process.env.DISCORD_TOKEN)
+  console.log('Logged in: ', login)
+
+  try {
+    client.on('ready', async () => {
+      const guild = await client.guilds.fetch(process.env.GUILD_ID)
+      const members = await guild.members.fetch()
+      console.log('Guild members...:')
+      for (const member of members) {
+        console.log(`id: ${ member[1].user.id } username: ${ member[1].user.username } discriminator: ${ member[1].user.discriminator }`)
+      }
+    })
+  }
+  catch(err) {
+    console.log('='.repeat(30))
+    console.log('Error retrieving Guild Members:')
+    console.log(err)
+    await client.destroy() // Terminate this Discord bot
+  }
+
 }
 
 export default extractDiscordUsers
